9b668221386635395ddb4fcd06f288dbbdb212ae
NekoQ <bitca.marius@isa.utm.md>
2019-12-25 20:06:19 +0200

    First

--- blitz.c
+++ blitz.c
@@ -35,7 +35,7 @@ int main()
 //    printf("%d ",max_sum_triangles(a));
 
 
-//    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++){
 //        for(int j=0;j<m;j++)
 //            printf("%d ",a[i][j]);
 //        printf("\n");
